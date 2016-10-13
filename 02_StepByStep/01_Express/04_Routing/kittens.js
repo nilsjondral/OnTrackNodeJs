f05fb1e28ad312574023f612a840bf8428146b84
nilsjondral <nils.jondral@telenet.be>
2016-10-13 15:34:34 +0200

    04_Routing kittens removed middleware

--- 02_StepByStep/01_Express/04_Routing/kittens.js
+++ 02_StepByStep/01_Express/04_Routing/kittens.js
@@ -2,18 +2,14 @@
 // var express = require('express');
 // var router = express.Router();
 
-// // Middleware
-// router.use(function(req, res, next) {
-//     console.log('kittens, kittens everywhere!');
-//     next();
-// });
-
 // router.get('/', function(req, res) {
 //     res.send('I like kittens');
 // });
 
 // router.get('/kitten', function(req, res){
-//     res.sendFile(__dirname + '/public/kitten.jpg', function(err) { console.log(err); });
+//     res.sendFile(__dirname + '/public/kitten.jpg', function(err) { 
+//         console.log(err); 
+//     });
 // });
 
 // module.exports = router;
--- 03_Finished/01_Express/04_Routing/kittens.js
+++ 03_Finished/01_Express/04_Routing/kittens.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// Middleware
-router.use(function(req, res, next) {
-    console.log('kittens, kittens everywhere!');
-    next();
-});
-
 router.get('/', function(req, res) {
     res.send('I like kittens');
 });
